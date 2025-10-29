0120032c94745e514b64e0b82d974bca8320c5ff
LivanKov <ilomakov4@gmail.com>
2025-10-29 11:47:22 +0100

    adjust to use sinc within the RCos pulse

--- src/pulse/RCos.m
+++ src/pulse/RCos.m
@@ -1,13 +1,12 @@
-function out = RCos(x,rolloff, a, mid, width, toPlot)
+function out = RCos(x,rolloff, toPlot)
 arguments
     x double
     rolloff (1,1) double = 1
-    a double = 1
-    mid (1,1) double = 0
-    width (1,1) double = 1
     toPlot string = 'false'
 end
-    out = sin(pi*x)/pi*x * cos(roloff*pi*x)/(1-4*rolloff^2*x^2)
+    sinc = Sinc(x);
+
+    out = sinc .* cos(rolloff * pi * x)./(1 - 4*rolloff^2 * x .^ 2)
 
     if toPlot == 't' | toPlot == "true"
         plot(x, out, 'Color', 'y', 'LineWidth', 1.5);
