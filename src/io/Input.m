bdbf77f39a05fd8dc143bcf5d4131ad3f3b93664
LivanKov <ilomakov4@gmail.com>
2025-11-09 15:32:58 +0100

    add function to read input in binary format

--- src/io/Input.m
+++ src/io/Input.m
@@ -2,18 +2,22 @@ classdef Input < handle
     properties
     end
 
-    methods
+    methods(Static)
         function bin_stream = create(path)
             arguments
                 path string
             end
             
-            fid = fopen(path);
+            fid = fopen(path, 'rb');
 
             if (fid ~= -1)
-                bin_stream = fread(fid, "bit1")';
+                bin_stream = fread(fid, "ubit1", 'b')';
+                len = length(bin_stream);
+                fprintf(1, "Read %s\nSize:\n   %d bits\n   %d bytes\n", path, len, len/8);
+                fclose(fid);
             else 
                 bin_stream = [];
+                fprintf(2, "Error: the input file does not exist or could not be opened\n");
             end
         end
 
